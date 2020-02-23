73972da90c02ece66b2b128da7bc873e378beca8
Fama Camara <famacama@e-r5-p2.s19.be>
2020-02-23 17:03:33 +0100

    ok

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: famacama <famacama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 14:26:26 by famacama          #+#    #+#             */
-/*   Updated: 2020/01/24 14:08:58 by famacama         ###   ########.fr       */
+/*   Updated: 2020/02/03 11:36:59 by famacama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (((start + len) > ft_strlen(s)) && start < ft_strlen(s))
 		len = ft_strlen(s) - start;
 	if (start > ft_strlen(s))
-		return (ft_strdup(""));
+		len = 0;
 	if (!(tab = malloc(sizeof(char) * len + 1)))
 		return (NULL);
 	while (s[start] && len != 0)
 	{
 		tab[i] = s[start];
-		i++;
 		start++;
+		i++;
 		len--;
 	}
 	tab[i] = '\0';
